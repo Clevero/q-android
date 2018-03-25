40e35eb5d6047d4b22da26d93209bad213c0cee7
Marcel Caspar <m.caspar@live.de>
2018-03-25 17:25:51 +0200

    add some custom data like categories and custom post meta for Orte

--- php/add-custom-data.php
+++ php/add-custom-data.php
@@ -10,7 +10,17 @@ function wpak_add_custom_data( $post_data, $post, $component ) {
     // Add subhead. Expected as a post custom field.
     // Usage in app's templates: <%= post.subhead %>
     $post_data['subhead'] = get_post_meta($post->ID, 'subhead', true);
-
+	$post_data['adresse'] = get_post_meta($post->ID, 'adresse', true);
+	$post_data['zusatz'] = get_post_meta($post->ID, 'zusatz', true);
+	$post_data['next_bus1'] = get_post_meta($post->ID, 'next_bus1', true);
+	$post_data['busverkehr1'] = get_post_meta($post->ID, 'busverkehr1', true);
+	$post_data['next_bus2'] = get_post_meta($post->ID, 'next_bus2', true);
+	$post_data['busverkehr2'] = get_post_meta($post->ID, 'busverkehr2', true);
+	
+	$post_data['next_bus2_entfernung'] = get_post_meta($post->ID, 'next_bus2_entfernung', true);
+	$post_data['next_bus1_entfernung'] = get_post_meta($post->ID, 'next_bus1_entfernung', true);
+	
+	
     // Add post thumbnail caption.
     // Usage in app's templates: <%= post.thumbnail.caption %>
     $thumbnail_id = get_post_thumbnail_id( $post->ID );
@@ -22,10 +32,26 @@ function wpak_add_custom_data( $post_data, $post, $component ) {
 			}
 		}
 	}
+	
+	$taxonomy = 'category'; // We search for categories
+    $terms = get_the_terms( $post->ID, $taxonomy ); // Get the post categories
+    $cat_list = wp_list_pluck( $terms, 'slug' ); // Get an array of slugs only
+    // Add category list to post data returned by the web service
+    $post_data['categories'] = $cat_list;
+
     
+	
     return $post_data; // Return the modified $post_data
 
+	
+	
+	
 }
 
 add_filter( 'wpak_post_data', 'wpak_add_custom_data', 10, 3 );
+
+
+
+
+
 ?>
